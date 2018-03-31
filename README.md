450698b583b5f4a7d830e520f83e9a3ebec135ae
stefanos1316 <stefanos1316@gmail.com>
2018-03-31 23:11:29 +0300

    Tools reports normally

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # validateLinks
 A repository with purpose on reporting if the existing links in a users repository are valid.
+
+Our mission here is to provide a tool that can check link found not only in html documents but any kind of. 
+Also, we extend the features of our tool to perform First Depth Search to identify even more links found under GitHub repositories.
+
+
+# Works for
+
+Currently, our tools works for GitHub repostiroes. 
+However, it can also be applied on local repositories.
--- validate_all.sh
+++ validate_all.sh
@@ -7,59 +7,10 @@
 DIR=$1
 LINKDIR=$DIR
 
-STATUS_000=0
-
-# Variables of 2xx Success
-STATUS_200=0
-STATUS_201=0
-STATUS_202=0
-STATUS_203=0
-STATUS_204=0
-STATUS_205=0
-STATUS_206=0
-STATUS_207=0
-STATUS_208=0
-STATUS_226=0
-
-# Variables for 3xx Redirectrion
-STATUS_300=0
-STATUS_301=0
-STATUS_302=0
-STATUS_303=0
-STATUS_304=0
-STATUS_305=0
-STATUS_306=0
-STATUS_307=0
-STATUS_308=0
-
-# Variables for 4xx Client errors
-STATUS_400=0
-STATUS_401=0
-STATUS_402=0
-STATUS_403=0
-STATUS_404=0
-STATUS_405=0
-STATUS_406=0
-STATUS_407=0
-STATUS_408=0
-STATUS_409=0
-STATUS_410=0
-STATUS_411=0
-STATUS_412=0
-STATUS_413=0
-STATUS_414=0
-STATUS_415=0
-STATUS_416=0
-STATUS_417=0
-STATUS_418=0
-STATUS_421=0
-STATUS_422=0
-STATUS_423=0
-STATUS_424=0
-STATUS_426=0
-STATUS_431=0
-STATUS_451=0
-STATUS_428=0
+STATUS_SUCCESS=0
+STATUS_REDIRECT=0
+STATUS_CLIENT_ERROR=0
+STATUS_SERVER_ERROR=0
 
 
 # Function declarations
@@ -75,7 +26,9 @@ function depth {
 
 function traverse {
 
-	ls "$1" | while read i
+	# Fix this one ...
+	# ls "$1" | while read i
+	while read i; 
 	do
 		depth $2
 		if [ -d "$1/$i" ]
@@ -87,21 +40,45 @@ function traverse {
 			# Calling function to check the status of the $i file's link 
 			getLinkStatus $1/$i
 		fi
-	done
+	done< <(ls "$1")
 }
 
 function getLinkStatus {
 
 while read LINE; do
-  curl -o /dev/null --silent --head --write-out "|---%{http_code}---| $LINE\n" "$LINE";
-  #increaseStatus $LINE
-done< <(egrep -ro 'https?://[^ ]+' $1 | sort | awk -F ":" '{print $1":"$2}' | tr -d '}' | tr -d ',' | tr -d '"' | tr -d ')' | tr -d '(' | tr -d ';' | sed 's/\.$//' | uniq )
+   URL=$(curl -o /dev/null --silent --head --write-out "|%{http_code}| $LINE\n" "$LINE")
+   echo $URL 
+   increaseStatus $URL
+done< <(egrep -ro 'https?://[^ ]+' $1 | sort | awk -F ":" '{print $1":"$2}' | tr -d '}*' | tr -d ',' | tr -d '"' | tr -d ')' | tr -d '(' | tr -d ';' | sed 's/\.$//' | uniq )
 
 }
 
+function increaseStatus {
 
+	STATUS=$(echo $1 | awk -F "|" '{print $2}')
 
-function printFinalReport {
+	# This long case statement is responsible for increasing the STATUS variable counts
+ 	if [ $STATUS -lt 300 ]; then
+		((++STATUS_SUCCESS))
+	elif [ $STATUS -lt 400 ]; then
+		((++STATUS_REDIRECT))
+	elif [ $STATUS -lt 500 ]; then
+		 ((++STATUS_CLIENT_ERROR))
+	elif [ $STATUS -lt 600 ]; then
+		 ((++STATUS_SERVER_ERROR))
+	else
+		echo "Such status don't exist."
+	fi
+}
+
+function printReports {
+echo ""
+echo "[Report] Total link's STATUS"
+echo "============================"
+echo "$STATUS_SUCCESS Success links"
+echo "$STATUS_REDIRECT Redirection links"
+echo "$STATUS_CLIENT_ERROR Client error links"
+echo "$STATUS_SERVER_ERROR Server error links"
 
 }
 
@@ -113,5 +90,7 @@ traverse . 0
 else
 traverse $1 0
 fi	
+
+printReports
 
 exit
